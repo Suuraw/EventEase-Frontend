d62f706ea6e9ec7137bce91288dc32fe18363ca9
Sujay kumar <sujayraw13@gmail.com>
2025-02-09 11:56:17 +0530

    add the logic to handle cold start of the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Dashboard from "./components/pages/Dashboard";
@@ -6,6 +6,9 @@ import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import ProfileSection from "./components/pages/Profile";
 import MyEvent from "./components/pages/MyEvent";
+import axios from "axios";
+const SERVER_URL = import.meta.env.VITE_SERVER_API + "/api/emptyReq";
+// const SERVER_URL = "http://localhost:3000/api/emptyReq";
 
 // Protected Route Component
 const ProtectedRoute = ({ element }) => {
@@ -14,6 +17,21 @@ const ProtectedRoute = ({ element }) => {
 };
 
 function App() {
+  const hitRender = async () => {
+    try {
+      await axios.get(SERVER_URL, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.log("Render is booting up,Wait a bit");
+    }
+  };
+
+  useEffect(() => {
+    hitRender();
+  }, []);
   const [myevent, setMyEvent] = useState([]);
   const [eventStatus, setEventStatus] = useState(false);
 
@@ -24,9 +42,39 @@ function App() {
       <Route path="/register" element={<Register />} />
 
       {/* Protected Routes */}
-      <Route path="/dashboard" element={<ProtectedRoute element={<Dashboard setEventStatus={setEventStatus} eventStatus={eventStatus} />} />} />
-      <Route path="/profile" element={<ProtectedRoute element={<ProfileSection myevent={myevent} />} />} />
-      <Route path="/myevent" element={<ProtectedRoute element={<MyEvent setMyEvent={setMyEvent} setEventStatus={setEventStatus} eventStatus={eventStatus} />} />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute
+            element={
+              <Dashboard
+                setEventStatus={setEventStatus}
+                eventStatus={eventStatus}
+              />
+            }
+          />
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute element={<ProfileSection myevent={myevent} />} />
+        }
+      />
+      <Route
+        path="/myevent"
+        element={
+          <ProtectedRoute
+            element={
+              <MyEvent
+                setMyEvent={setMyEvent}
+                setEventStatus={setEventStatus}
+                eventStatus={eventStatus}
+              />
+            }
+          />
+        }
+      />
     </Routes>
   );
 }
